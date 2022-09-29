f99dd762793a6f559cf1a56047c4bb25cdd240c2
Dima <dmitriiburiachek@gmail.com>
2022-09-29 14:01:19 +0300

    extended group with field description

--- admin_dashboard/dashboard/models.py
+++ admin_dashboard/dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
-# Create your models here.
+
+if not hasattr(Group, 'description'):
+    field = models.TextField(blank=True, null=True)
+    field.contribute_to_class(Group, 'description')
+
