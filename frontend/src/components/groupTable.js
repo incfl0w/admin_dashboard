45a21aa840c802863a10aa18d3871f621769810a
Dima <dmitriiburiachek@gmail.com>
2022-09-29 12:38:23 +0300

    tables error fixed

--- frontend/src/components/groupTable.js
+++ frontend/src/components/groupTable.js
@@ -121,16 +121,11 @@ function EnhancedTableHead(props) {
 }
 
 EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
 };
-
-
-
 
 
 export default function GroupTable() {
--- frontend/src/components/userTable.js
+++ frontend/src/components/userTable.js
@@ -127,9 +127,7 @@ function EnhancedTableHead(props) {
 }
 
 EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
