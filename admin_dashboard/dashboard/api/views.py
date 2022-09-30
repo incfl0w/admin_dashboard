427b30cb110dcda110efa40e2ef7743f1fc00035
Dima <dmitriiburiachek@gmail.com>
2022-09-30 14:15:39 +0300

    django rest filters

--- admin_dashboard/dashboard/api/views.py
+++ admin_dashboard/dashboard/api/views.py
@@ -2,9 +2,13 @@ from rest_framework import generics
 from django.http import JsonResponse
 from django.contrib.auth.models import User, Group
 from .serializers import UserCreateSerializer, UserUpdateSerializer, GroupSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .service import UserFilter, GroupFilter
 
 class UserList(generics.ListCreateAPIView):
     serializer_class = UserCreateSerializer
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = UserFilter
     
     def get_queryset(self):
         return User.objects.all()
@@ -17,6 +21,8 @@ class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     
 class GroupList(generics.ListCreateAPIView):
     serializer_class = GroupSerializer
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = GroupFilter
     
     def get_queryset(self):
         return Group.objects.all()
